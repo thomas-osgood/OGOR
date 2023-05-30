ec5a313327aae336c4de39b117cb8932a2748f92
Thomas Osgood <thomasosgood@redacted.local>
2023-05-29 22:41:04 -0400

    added comments to IPGrabber struct definition

--- networking/ipgrabber/structures.go
+++ networking/ipgrabber/structures.go
@@ -5,7 +5,13 @@ import "net"
 // structure representing the main object that will search for
 // and hold the discovered addresses and interfaces.
 type IPGrabber struct {
+
+	// slice holding all the discovered network interfaces on
+	// the current system. this excludes the loopback interface.
 	Interfaces []net.Interface
+
+	// slice holding all the discovered IP addresses for all
+	// the interfaces in the Interfaces slice.
 	Discovered []net.IP
 }
 
