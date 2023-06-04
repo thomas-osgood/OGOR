e0f26172f49d3ff482d6e409c41555611d5ac2f4
Thomas Osgood <thomasosgood@redacted.local>
2023-06-04 18:00:08 -0400

    output functions now attached to new Outputter structure

--- output/output.go
+++ output/output.go
@@ -2,37 +2,37 @@ package output
 
 import "fmt"
 
-func SucMsg(message string) {
+func (o *Outputter) SucMsg(message string) {
 	fmt.Printf("%s %s\n", SUCCESS_SEQ, message)
 	return
 }
 
-func ErrMsg(message string) {
+func (o *Outputter) ErrMsg(message string) {
 	fmt.Printf("%s %s\n", FAIL_SEQ, message)
 	return
 }
 
-func InfMsg(message string) {
+func (o *Outputter) InfMsg(message string) {
 	fmt.Printf("%s %s\n", INFO_SEQ, message)
 	return
 }
 
-func InfMsgNB(message string) {
+func (o *Outputter) InfMsgNB(message string) {
 	fmt.Printf("%s %s\r", INFO_SEQ, message)
 	return
 }
 
-func SysMsg(message string) {
+func (o *Outputter) SysMsg(message string) {
 	fmt.Printf("%s %s\n", SYS_SEQ, message)
 	return
 }
 
-func SysMsgNB(message string) {
+func (o *Outputter) SysMsgNB(message string) {
 	fmt.Printf("%s %s\r", SYS_SEQ, message)
 	return
 }
 
-func WrnMsg(message string) {
+func (o *Outputter) WrnMsg(message string) {
 	fmt.Printf("%s %s\n", WARNING_SEQ, message)
 	return
 }
