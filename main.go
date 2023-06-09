d8f4f88c50b26d439e9a999b376bf3209e584a03
Thomas Osgood <thomasosgood@redacted.local>
2023-06-08 22:51:58 -0400

    added import

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	_ "github.com/thomas-osgood/OGOR/commandline/arguments"
+	_ "github.com/thomas-osgood/OGOR/misc/generators"
 	_ "github.com/thomas-osgood/OGOR/networking/ipgrabber"
 	_ "github.com/thomas-osgood/OGOR/networking/ipinfo"
 	_ "github.com/thomas-osgood/OGOR/output"
