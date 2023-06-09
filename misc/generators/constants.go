3838f41a502a9a1047f02394efad1d4d3d0eb110
Thomas Osgood <thomasosgood@redacted.local>
2023-06-08 22:51:10 -0400

    added constants file

---
+++ misc/generators/constants.go
@@ -0,0 +1,9 @@
+package generators
+
+// default value for the minimum length of randomly
+// generated strings.
+const DEFAULT_RAND_MIN int = 5
+
+// default value for the maximum length of randomly
+// generated strings.
+const DEFAULT_RAND_MAX int = 15
