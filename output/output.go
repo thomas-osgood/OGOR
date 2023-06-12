034b3651979ec984d6fc9ac1ce59906049fec038
Thomas Osgood <thomasosgood@redacted.local>
2023-06-11 21:12:46 -0400

    changed message inputs to interface{} from string

--- output/output.go
+++ output/output.go
@@ -2,37 +2,37 @@ package output
 
 import "fmt"
 
-func (o *Outputter) SucMsg(message string) {
-	fmt.Printf("%s%s %s\n", CLEAR_LINE, SUCCESS_SEQ, message)
+func (o *Outputter) SucMsg(message interface{}) {
+	fmt.Printf("%s%s %v\n", CLEAR_LINE, SUCCESS_SEQ, message)
 	return
 }
 
-func (o *Outputter) ErrMsg(message string) {
-	fmt.Printf("%s%s %s\n", CLEAR_LINE, FAIL_SEQ, message)
+func (o *Outputter) ErrMsg(message interface{}) {
+	fmt.Printf("%s%s %v\n", CLEAR_LINE, FAIL_SEQ, message)
 	return
 }
 
-func (o *Outputter) InfMsg(message string) {
-	fmt.Printf("%s%s %s\n", CLEAR_LINE, INFO_SEQ, message)
+func (o *Outputter) InfMsg(message interface{}) {
+	fmt.Printf("%s%s %v\n", CLEAR_LINE, INFO_SEQ, message)
 	return
 }
 
-func (o *Outputter) InfMsgNB(message string) {
-	fmt.Printf("%s%s %s\r", CLEAR_LINE, INFO_SEQ, message)
+func (o *Outputter) InfMsgNB(message interface{}) {
+	fmt.Printf("%s%s %v\r", CLEAR_LINE, INFO_SEQ, message)
 	return
 }
 
-func (o *Outputter) SysMsg(message string) {
-	fmt.Printf("%s%s %s\n", CLEAR_LINE, SYS_SEQ, message)
+func (o *Outputter) SysMsg(message interface{}) {
+	fmt.Printf("%s%s %v\n", CLEAR_LINE, SYS_SEQ, message)
 	return
 }
 
-func (o *Outputter) SysMsgNB(message string) {
-	fmt.Printf("%s%s %s\r", CLEAR_LINE, SYS_SEQ, message)
+func (o *Outputter) SysMsgNB(message interface{}) {
+	fmt.Printf("%s%s %v\r", CLEAR_LINE, SYS_SEQ, message)
 	return
 }
 
-func (o *Outputter) WrnMsg(message string) {
-	fmt.Printf("%s%s %s\n", CLEAR_LINE, WARNING_SEQ, message)
+func (o *Outputter) WrnMsg(message interface{}) {
+	fmt.Printf("%s%s %v\n", CLEAR_LINE, WARNING_SEQ, message)
 	return
 }
