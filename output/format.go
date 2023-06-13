3eb4ce6fc1861974d8efc8e4300f04631d962d68
Thomas Osgood <thomasosgood@redacted.local>
2023-06-12 20:42:50 -0400

    added 4 new functions to formatter

--- output/format.go
+++ output/format.go
@@ -29,3 +29,31 @@ func (f *Formatter) BlueText(text interface{}) (formatted string) {
 	formatted = fmt.Sprintf("%s%v%s", BLUE_SEQ, text, ANSI_RESET)
 	return formatted
 }
+
+// function designed to take an input and return a formatted Success string.
+// this string output will be formatted the same as an Outputter's ErrMsg.
+func (f *Formatter) ErrMsg(text interface{}) (formatted string) {
+	formatted = fmt.Sprintf("%s%s %v\n", CLEAR_LINE, FAIL_SEQ, text)
+	return formatted
+}
+
+// function designed to take an input and return a formatted Success string.
+// this string output will be formatted the same as an Outputter's SucMsg.
+func (f *Formatter) SucMsg(text interface{}) (formatted string) {
+	formatted = fmt.Sprintf("%s%s %v\n", CLEAR_LINE, SUCCESS_SEQ, text)
+	return formatted
+}
+
+// function designed to take an input and return a formatted Success string.
+// this string output will be formatted the same as an Outputter's InfMsg.
+func (f *Formatter) InfMsg(text interface{}) (formatted string) {
+	formatted = fmt.Sprintf("%s%s %v\n", CLEAR_LINE, INFO_SEQ, text)
+	return formatted
+}
+
+// function designed to take an input and return a formatted Success string.
+// this string output will be formatted the same as an Outputter's SysMsg.
+func (f *Formatter) SysMsg(text interface{}) (formatted string) {
+	formatted = fmt.Sprintf("%s%s %v\n", CLEAR_LINE, SYS_SEQ, text)
+	return formatted
+}
