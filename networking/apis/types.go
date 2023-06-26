35580fc366b940fdda1e35f29956d888d997cfae
Thomas Osgood <thomasosgood@redacted.local>
2023-06-25 22:50:58 -0400

    added MiddlewareOptsFunc type alias

--- networking/apis/types.go
+++ networking/apis/types.go
@@ -5,3 +5,7 @@ import "net/http"
 // alias of http request function that returns an error. this will
 // be handled by middleware and is used for more control/neater code.
 type APIFunc func(http.ResponseWriter, *http.Request) error
+
+// alias of a function that is used to manipulate the various parts
+// of a MiddlewareOptions object.
+type MiddlewareOptsFunc func(*MiddlewareOptions) error
