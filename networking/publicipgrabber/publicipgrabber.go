8c9ce3ed80a4fa77ddc70852e1e370b44961e1b3
Thomas Osgood <thomasosgood@redacted.local>
2023-08-06 16:36:09 -0400

    added ability to query information for a specified IP address.

--- networking/publicipgrabber/publicipgrabber.go
+++ networking/publicipgrabber/publicipgrabber.go
@@ -5,23 +5,73 @@
 package publicipgrabber
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"net/textproto"
 )
+
+// function designed to query api.whatismyip.com and get information
+// related to a given IP address.
+func (ipg *PublicIPGrabber) GetIPInformation(targetip string) (ipinformation *AppResponse, err error) {
+	var bodycontent []byte
+	var databuffer *bytes.Buffer = new(bytes.Buffer)
+	var datawriter *multipart.Writer = multipart.NewWriter(databuffer)
+	var req *http.Request
+	var resp *http.Response
+	var targeturl string = fmt.Sprintf("%s/app.php", BASE_URL)
+
+	ipinformation = new(AppResponse)
+
+	err = ipg.setLookupMultipartData(datawriter, targetip)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err = http.NewRequest(http.MethodPost, targeturl, databuffer)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", datawriter.FormDataContentType())
+
+	err = ipg.setRequestHeaders(req)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err = ipg.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= http.StatusBadRequest {
+		return nil, errors.New(fmt.Sprintf("error getting ip information: %s", resp.Status))
+	}
+
+	bodycontent, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(bodycontent, ipinformation)
+	if err != nil {
+		return nil, err
+	}
+
+	return ipinformation, nil
+}
 
 // function designed to query api.whatismyip.com and
 // pull down the public IP address information for the
 // machine executing the program.
 func (ipg *PublicIPGrabber) GetMyIPInformation() (err error) {
-	const acceptHeader string = "*/*"
-	const acceptEncoding string = "json"
-	const origin string = "https://www.whatismyip.com"
-	const referer string = "https://www.whatismyip.com"
-	const secgpc string = "1"
-	const useragent string = "grabber"
 
 	var bodycontent []byte
 	var jsonResp PublicIPInfo = PublicIPInfo{}
@@ -34,12 +84,10 @@ func (ipg *PublicIPGrabber) GetMyIPInformation() (err error) {
 		return err
 	}
 
-	req.Header.Set("Accept-Encoding", acceptEncoding)
-	req.Header.Set("Accept", acceptHeader)
-	req.Header.Set("Origin", origin)
-	req.Header.Set("Referer", referer)
-	req.Header.Set("Sec-GPC", secgpc)
-	req.Header.Set("User-Agent", useragent)
+	err = ipg.setRequestHeaders(req)
+	if err != nil {
+		return err
+	}
 
 	resp, err = ipg.client.Do(req)
 	if err != nil {
@@ -66,6 +114,53 @@ func (ipg *PublicIPGrabber) GetMyIPInformation() (err error) {
 	}
 
 	ipg.PublicIP = jsonResp
+
+	return nil
+}
+
+// function designed to setup the multipart data used in
+// a call to api.whatismyip.com when querying information
+// related to a given IP address.
+func (ipg *PublicIPGrabber) setLookupMultipartData(datawriter *multipart.Writer, targetip string) (err error) {
+	defer datawriter.Close()
+
+	var header textproto.MIMEHeader = make(textproto.MIMEHeader)
+	var part io.Writer
+
+	header.Set("Content-Disposition", "form-data; name=\"action\"")
+
+	part, err = datawriter.CreatePart(header)
+	if err != nil {
+		return err
+	}
+	part.Write([]byte("ip-lookup"))
+
+	header.Set("Content-Disposition", "form-data; name=\"ip\"")
+	part, err = datawriter.CreatePart(header)
+	if err != nil {
+		return err
+	}
+	part.Write([]byte(targetip))
+
+	return nil
+}
+
+// function designed to set the proper header values
+// for a request to api.whatismyip.com.
+func (ipg *PublicIPGrabber) setRequestHeaders(req *http.Request) (err error) {
+	const acceptHeader string = "*/*"
+	const acceptEncoding string = "json"
+	const origin string = "https://www.whatismyip.com"
+	const referer string = "https://www.whatismyip.com"
+	const secgpc string = "1"
+	const useragent string = "grabber"
+
+	req.Header.Set("Accept-Encoding", acceptEncoding)
+	req.Header.Set("Accept", acceptHeader)
+	req.Header.Set("Origin", origin)
+	req.Header.Set("Referer", referer)
+	req.Header.Set("Sec-GPC", secgpc)
+	req.Header.Set("User-Agent", useragent)
 
 	return nil
 }
--- networking/publicipgrabber/structs.go
+++ networking/publicipgrabber/structs.go
@@ -2,6 +2,30 @@ package publicipgrabber
 
 import "net/http"
 
+// structure representing the response from api.whatismyip.com/app.php
+// when querying information related to an IP address.
+type AppResponse struct {
+	// data returned from ip2location.com query made
+	// by api.whatismyip.com/app.php.
+	Ip2location LocationResponse `json:"ip2location.com"`
+
+	// data returned from ipdata.co query made
+	// by api.whatismyip.com/app.php.
+	Ipdata LocationResponse `json:"ipdata.co"`
+}
+
+// structure defining the IP2Location and IPData
+// JSON returns from app.php.
+type LocationResponse struct {
+	Asn        string `json:"asn"`
+	City       string `json:"city"`
+	Region     string `json:"region"`
+	Country    string `json:"country"`
+	PostalCode string `json:"postal_code"`
+	Isp        string `json:"isp"`
+	TimeZone   string `json:"time_zone"`
+}
+
 // structure defining a PublicIPGrabber object. this
 // will have associated functions to query the site
 // api.whatismyip.com and grab the public IP info.
