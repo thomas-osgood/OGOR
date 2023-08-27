b8a6047697c6ab8de6f5b07479102357e409e163
Thomas Osgood <thomasosgood@redacted.local>
2023-08-27 00:49:51 -0400

    updated main imports

--- main.go
+++ main.go
@@ -6,12 +6,23 @@ package main
 
 import (
 	_ "github.com/thomas-osgood/OGOR/commandline/arguments"
+	_ "github.com/thomas-osgood/OGOR/crypto/decoders"
+	_ "github.com/thomas-osgood/OGOR/crypto/encoders"
+	_ "github.com/thomas-osgood/OGOR/misc/crosscompile"
 	_ "github.com/thomas-osgood/OGOR/misc/error-handling"
 	_ "github.com/thomas-osgood/OGOR/misc/generators"
 	_ "github.com/thomas-osgood/OGOR/networking/apis"
 	_ "github.com/thomas-osgood/OGOR/networking/ipgrabber"
 	_ "github.com/thomas-osgood/OGOR/networking/ipinfo"
+	_ "github.com/thomas-osgood/OGOR/networking/proxyscrape"
+	_ "github.com/thomas-osgood/OGOR/networking/publicipgrabber"
+	_ "github.com/thomas-osgood/OGOR/networking/scanning/dnsenum"
+	_ "github.com/thomas-osgood/OGOR/networking/validations"
 	_ "github.com/thomas-osgood/OGOR/output"
+	_ "github.com/thomas-osgood/OGOR/protobufs/definitions/common"
+	_ "github.com/thomas-osgood/OGOR/protobufs/definitions/filehandler"
+	_ "github.com/thomas-osgood/OGOR/protobufs/filehandler"
+	_ "github.com/thomas-osgood/OGOR/protobufs/general"
 	_ "github.com/thomas-osgood/OGOR/vulns/lfi"
 )
 
