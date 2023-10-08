26e7fb85b36039df55eec6fe2966c94541cc0545
Thomas Osgood <thomasosgood@redacted.local>
2023-10-08 16:14:23 -0400

    began GetCPUInfo function

--- sysinfo/general/functions_linux.go
+++ sysinfo/general/functions_linux.go
@@ -3,7 +3,11 @@ package general
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -55,4 +59,95 @@ func EnumMounts() (mounts []MountInfo, err error) {
 	}
 
 	return mounts, nil
+}
+
+// function deisgned to read the cpuinfo file and pull
+// out information of interest. this will help give a
+// better understanding of the machine's CPU(s).
+func GetCPUInfo() (info AllCpuInfo, err error) {
+	var proccntpat string = "processor\\s+:\\s?[0-9]+"
+	var procmodpat string = "model\\sname\\s+:.*"
+	var procspdpat string = "cpu\\sMHz\\s+:\\s?[0-9.]+"
+	var fptr *os.File
+	var idx int
+	var match []byte
+	var matches [][]byte
+	var numprocs int
+	var processor []byte
+	var processors [][]byte
+	var rawdata []byte
+	var re *regexp.Regexp
+	var repm *regexp.Regexp
+	var reps *regexp.Regexp
+	var splitmatch [][]byte
+	const target string = "/proc/cpuinfo"
+
+	// initialize Cpus slice in AllCpuInfo struct
+	info.Cpus = make([]CpuInfo, 0)
+
+	fptr, err = os.Open(target)
+	if err != nil {
+		return AllCpuInfo{}, err
+	}
+	defer fptr.Close()
+
+	rawdata, err = io.ReadAll(fptr)
+	if err != nil {
+		return AllCpuInfo{}, err
+	}
+
+	rawdata = bytes.TrimSpace(rawdata)
+
+	info = AllCpuInfo{
+		Raw: rawdata,
+	}
+
+	// setup regex to pullout the different processors
+	re, err = regexp.Compile(proccntpat)
+	if err != nil {
+		return info, err
+	}
+
+	// pull out all processors discovered in the file
+	matches = re.FindAll(rawdata, -1)
+	if matches == nil {
+		numprocs = 0
+	} else {
+		numprocs = len(matches)
+	}
+
+	info.ProcessorCount = numprocs
+
+	// setup regex to pull out the processor model
+	repm, err = regexp.Compile(procmodpat)
+	if err != nil {
+		return info, err
+	}
+
+	// setup regex to pull out the processor speed
+	reps, err = regexp.Compile(procspdpat)
+	if err != nil {
+		return info, err
+	}
+
+	processors = bytes.Split(rawdata, []byte("\n\n"))
+	for idx, processor = range processors {
+		info.Cpus = append(info.Cpus, CpuInfo{})
+
+		matches = repm.FindAll(processor, -1)
+		if matches != nil {
+			splitmatch = bytes.Split(matches[0], []byte(":"))
+			match = bytes.TrimSpace(splitmatch[len(splitmatch)-1])
+			info.Cpus[idx].ProcessorModel = string(match)
+		}
+
+		matches = reps.FindAll(processor, -1)
+		if matches != nil {
+			splitmatch = bytes.Split(matches[0], []byte(":"))
+			match = bytes.TrimSpace(splitmatch[len(splitmatch)-1])
+			info.Cpus[idx].ProcessorSpeed, err = strconv.ParseFloat(string(match), 64)
+		}
+	}
+
+	return info, nil
 }
