ff559360fdcdc0dc2118381d566e52fce706b4a8
Thomas Osgood <thomasosgood@redacted.local>
2023-10-08 16:11:10 -0400

    began cpuinfo structs

--- sysinfo/general/structs.go
+++ sysinfo/general/structs.go
@@ -1,6 +1,21 @@
 package general
 
 // structure designed to hold the information
+// related to all the CPUs on a machine.
+type AllCpuInfo struct {
+	Cpus           []CpuInfo
+	ProcessorCount int
+	Raw            []byte
+}
+
+// structure designed to hold the information
+// related to a single CPU (processor) on a machine.
+type CpuInfo struct {
+	ProcessorModel string
+	ProcessorSpeed float64
+}
+
+// structure designed to hold the information
 // related to a system mount.
 type MountInfo struct {
 	Name       string
