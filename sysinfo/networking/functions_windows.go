af6a45b88dec30e7b7f88ae613d18c463bd1dc7e
Thomas Osgood <thomasosgood@redacted.local>
2023-10-09 20:05:44 -0400

    added windows EnumDnsServers function

--- sysinfo/networking/functions_windows.go
+++ sysinfo/networking/functions_windows.go
@@ -3,9 +3,44 @@
 
 package networking
 
-// function designed to enumerate the DNS information
-// of a Windows machine and return the nameserver IPs.
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+)
+
+// function designed to read the resolv.conf file
+// and return the IP addresses of the dns nameservers.
+//
+// for ref:
+// https://kevincurran.org/com320/labs/dns.htm
 func EnumDnsServers() (servers []string, err error) {
+	var cmd *exec.Cmd
+	var cmdstr string = "cmd.exe"
+	var cmdarg []string = []string{"/C", "ipconfig", "/all"}
+	var dnsip string
+	var dnsips string
+	var outbytes []byte
+	var outsplit [][]byte
+
 	servers = make([]string, 0)
+
+	cmd = exec.Command(cmdstr, cmdarg...)
+	outbytes, err = cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+	outbytes = bytes.TrimSpace(outbytes)
+
+	outsplit = bytes.Split(outbytes, []byte("\n"))
+	for _, outbytes = range outsplit {
+		if bytes.Contains(outbytes, []byte("DNS Servers")) {
+			dnsips = string(bytes.Join(bytes.Split(outbytes, []byte(":"))[1:], []byte(",")))
+			for _, dnsip = range strings.Split(dnsips, ",") {
+				servers = append(servers, dnsip)
+			}
+		}
+	}
+
 	return servers, nil
 }
