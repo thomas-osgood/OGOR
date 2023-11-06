0108dcaf5f0026b2ca127e62f37f17164f09fcb8
Thomas Osgood <thomasosgood@redacted.local>
2023-11-05 20:30:48 -0500

    added reference in comment

--- sysinfo/firewalls/globals_windows.go
+++ sysinfo/firewalls/globals_windows.go
@@ -7,4 +7,6 @@ package firewalls
 // arguments that will be passed to
 // the command to enumerate the
 // firewall binaries.
+//
+// ref: https://ss64.com/nt/where.html
 var enumarguments []string = []string{"/R", "C:\\", ""}
