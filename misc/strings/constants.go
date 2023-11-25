5a9a0350e35a71ead1fa15474c2ed6f2de4ffbba
Thomas Osgood <thomasosgood@redacted.local>
2023-11-25 16:38:44 -0500

    added alphabet and digit constants to misc.

---
+++ misc/strings/constants.go
@@ -0,0 +1,10 @@
+package strings
+
+// digits
+const ASCII_DIGITS string = "0123456789"
+
+// lowercase letters
+const ASCII_LOWERCASE string = "abcdefghijklmnopqrstuvwxyz"
+
+// uppercase letters
+const ASCII_UPPERCASE string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
